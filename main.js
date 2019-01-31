4031ae3b7559cfea2096df91d235971382a8a3df
Erik Konner <erikkonner@gmail.com>
2019-01-30 17:12:27 -0700

    Added JS variables, updated buttons so they dont refresh the page

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var updateBtn = document.querySelector('.btn-update')
+var updateBtn = document.querySelector('.btn-update');
+var minRange = document.querySelector('#min-range');
+var maxRange = document.querySelector('#max-range');
+var lowEnd = document.querySelector('#low-end');
+var highEnd = document.querySelector('#high-end');
+
+updateBtn.addEventListener('click', function(event) {
+  event.preventdefault();
+  lowEnd.innerText = minRange.value;
+  highEnd.innerText = maxRange.value;
+  })
+
