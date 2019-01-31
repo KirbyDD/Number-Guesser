df3850afd9d613f517f02a14d81ba265b834bc66
DeMarcus Kirby <tkd024@gmail.com>
2019-01-31 10:51:40 -0700

    added JS var and updated HTML and CSS

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ var minRange = document.querySelector('#min-range');
 var maxRange = document.querySelector('#max-range');
 var lowEnd = document.querySelector('#low-end');
 var highEnd = document.querySelector('#high-end');
+var pOneName = document.querySelector('#player-one-name');
+var pTwoName = document.querySelector('#player-two-name');
+var pOneGuessInput = document.querySelector('#p1-input');
+var pTwoGuessInput = document.querySelector('#p2-input');
+
+
 
 updateBtn.addEventListener('click', function(event) {
+  // if(minRange.value != so && maxRange != NaN && maxRange > minRange){
   lowEnd.innerText = minRange.value;
   highEnd.innerText = maxRange.value;
+// } else {
+//   alert('Please correct values');
+// }
   event.preventdefault();
   })
 
